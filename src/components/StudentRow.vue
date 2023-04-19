d68ab283a80ca01605f4bc3cbb7f617e11d19a4f
Mr. 3rd <97487150+Mr-3rd@users.noreply.github.com>
2023-04-19 04:50:51 -0500

    Topic 10: Vue Components

--- src/components/StudentRow.vue
+++ src/components/StudentRow.vue
@@ -1,6 +1,5 @@
 <template>
 
-<!--  Comment to help fix wtf is wrong with my github-->
   <tr v-bind:class="{present: student.present, absent: !student.present}">
     <td>{{student.name}}</td>
     <td>{{student.starID}}</td>
