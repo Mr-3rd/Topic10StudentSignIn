5d2d8528ab3eaf2a00cbca70aa5ead69e2303062
Mr. 3rd <97487150+Mr-3rd@users.noreply.github.com>
2023-04-19 04:39:23 -0500

    Topic 10: Vue Components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,77 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <new-student-form v-on:student-added="newStudentAdded"></new-student-form>
+  <student-table v-bind:students="students"
+                 v-on:studentArrivedOrLeft="studentArrivedOrLeft"
+                 v-on:delete-student="studentDeleted"
+  ></student-table>
+  <student-message v-bind:student="latestStudent"
+  ></student-message>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NewStudentForm from './components/NewStudentForm.vue'
+import StudentMessage from './components/StudentMessage.vue'
+import StudentTable from './components/StudentTable.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NewStudentForm,
+    StudentMessage,
+    StudentTable,
+  },
+  data() {
+    return {
+      students: [],
+      latestStudent: {}
+    }
+  },
+  methods: {
+    newStudentAdded(student) {
+      this.students.push(student)
+      this.sortStudents();
+    },
+    sortStudents() {
+      // Sor the students array
+      this.students.sort(function (s1, s2) {
+        return s1.name.toLowerCase() > s2.name.toLowerCase() ? -1: 1
+    })
+    },
+    studentArrivedOrLeft(student, present) {
+      // Find student
+      // update attribute
+      let updateStudent = this.students.find( function (s) {
+        if (s.name === student.name && s.starID === student.starID) {
+          return true
+        }
+      })
+
+      if (updateStudent) {
+        updateStudent.present = present
+        this.latestStudent = updateStudent
+      }
+    },
+    studentDeleted(student) {
+
+      //This seems to be where my code will not delete the rom
+      this.students.filter( function(s) {
+        if (s !== student) {
+          return true
+        }
+      })
+
+      //Pressing the delete button will clear the last message
+      this.latestStudent = {}
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+@import "https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css";
+
 </style>
